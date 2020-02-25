cd350f7b5804c328320183c1169734af316083f0
MahmodMhamed <mahmod.mhamid@hotmail.com>
2020-02-25 15:17:23 +0200

    create rel#

---
+++ src/components/left_player.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const LeftPlayer = () => {
+  return (
+    <section className="player">
+      <div id="leftPlayer">
+        <img src="https://assets.pokemon.com/assets/cms2/img/misc/countries/mt/country_detail_pokemon.png"></img>
+        <h1>Mahmod</h1>
+      </div>
+    </section>
+  );
+};
+
+export default LeftPlayer;
