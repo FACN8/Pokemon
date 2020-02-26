9b92aafa4b12a5c176f01a80cdaf65864620d2c0
MahmodMhamed <mahmod.mhamid@hotmail.com>
2020-02-26 11:57:24 +0200

    add style to players components rel#3

--- src/components/battle_stats.js
+++ src/components/battle_stats.js
@@ -1,47 +1,37 @@
+import React from 'react';
+import { getPokemon } from '../utils/getPokemon';
+import { getMove } from '../utils/getMove';
 
-import React from "react";
-import { getPokemon } from "../utils/getPokemon";
-import { getMove } from "../utils/getMove";
-
-function Battle({ pokemon1,pokemon2,move }) {
-  var p1=100,p2=100;
+function Battle({ pokemon1, pokemon2, move }) {
+  var p1 = 100,
+    p2 = 100;
   var turn = true;
   React.useEffect(() => {
-    if(move==null) return;
-    if(turn){
-        p2-=move.power;
-    }
-    else{
-        p1-=move.power;
+    if (move == null) return;
+    if (turn) {
+      p2 -= move.power;
+    } else {
+      p1 -= move.power;
     }
     turn = !turn;
     return;
   }, [move]);
- 
+
   return (
-    <div>
-    <div id="lifeBar1"> 
-            <div id="firstPokemonLife">Life of pokemon1: {p1}</div> 
-      </div> 
-      <div id="lifeBar2"> 
-            <div id="secondPokemonLife">Life of pokemon2: {p2}</div> 
-      </div> 
-
+    <div className="stats">
+      <div className="lifeBar" id="lifeBar1">
+        <img className="lifeHeart" src="./life.png"></img>
+        <h3 id="firstPokemonLife">Pokemon - 1: {p1}%</h3>
       </div>
-
+      <div className="lifeBar" id="lifeBar2">
+        <h3 id="secondPokemonLife">Pokemon - 2: {p2}%</h3>
+        <img className="lifeHeart" src="./life.png"></img>
+      </div>
+    </div>
   );
 }
 
 export default Battle;
 
-
-
-
-
-
-
-
-
-
-// var width = (currentquestion+1)/maxquestions * 100;  
+// var width = (currentquestion+1)/maxquestions * 100;
 // element.style.width = width + '%';
--- src/components/right_player.js
+++ src/components/right_player.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const RightPlayer = ({ setPokemon, setMove }) => {
-  React.useEffect(()=>{
+  React.useEffect(() => {
     return;
-  },[])
+  }, []);
   return (
     <section className="player">
       <h1>2nd Player</h1>
@@ -11,13 +11,15 @@ const RightPlayer = ({ setPokemon, setMove }) => {
         <img src="https://sm.ign.com/t/ign_il/screenshot/g/grooky-is-/grooky-is-a-grass-type-chimp-pokemon-and-one-of-three-starte_jdxn.620.jpg"></img>
         <h1>Aysam</h1>
         <ul className="moves">
-          <li>1 - <button onClick={setMove({power: 20})}>Hello</button></li>
+          <li>
+            1 - <button onClick={() => setMove({ power: 20 })}>Hello</button>
+          </li>
           <li>2</li>
           <li>3</li>
           <li>4</li>
         </ul>
         <img className="pokeball" src="./pokeball.png"></img>
-              </div>
+      </div>
     </section>
   );
 };
